1765b4370655effd16bb1a2ffb0b4690552850aa
Alexandre Amor <dev@ralex.fr>
2022-04-07 17:03:10 +0200

    Ending chapter 4

--- README.md
+++ README.md
@@ -5,3 +5,29 @@ Playing with Rust while reading "[The Rust Programming Language](https://doc.rus
 ```bash
 rustup docs --book
 ```
+
+1. ✅ Getting Started
+2. ✅ Programming a Guessing Game
+3. ✅ Common Programming Concepts
+4. ✅ Understanding Ownership
+5. 🔲 Using Structs to Structure Related Data
+6. 🔲 Enums and Pattern Matching
+7. 🔲 Managing Growing Projects with Packages, Crates, and Modules
+8. 🔲 Common Collections
+9. 🔲 Error Handling
+10. 🔲 Generic Types, Traits, and Lifetimes
+11. 🔲 Writing Automated Tests
+12. 🔲 An I/O Project: Building a Command Line Program
+13. 🔲 Functional Language Features: Iterators and Closures
+14. 🔲 More about Cargo and Crates.io
+15. 🔲 Smart Pointers
+16. 🔲 Fearless Concurrency
+17. 🔲 Object Oriented Programming Features of Rust
+18. 🔲 Patterns and Matching
+19. 🔲 Advanced Features
+20. 🔲 Final Project: Building a Multithreaded Web Server
+21. 🔲 Appendix
+
+# Cargo
+
+[The Cargo Book](https://doc.rust-lang.org/cargo/index.html)
---
+++ slices/src/main.rs
@@ -0,0 +1,51 @@
+fn main() {
+    let sentence = String::from("Hello world blabla");
+
+    println!("First word end at position: {}", first_word(&sentence));
+    println!("Sliced sentence: word1: '{}', word2: '{}'", &sentence[..5], &sentence[6..]);
+
+    println!("First word: {}", first_word_new(&sentence));
+
+    let my_string = String::from("hello world");
+
+    // `first_word_new` works on slices of `String`s, whether partial or whole
+    let word = first_word_new(&my_string[0..6]);
+    let word = first_word_new(&my_string[..]);
+    // `first_word_new` also works on references to `String`s, which are equivalent
+    // to whole slices of `String`s
+    let word = first_word_new(&my_string);
+
+    let my_string_literal = "hello world";
+
+    // `first_word_new` works on slices of string literals, whether partial or whole
+    let word = first_word_new(&my_string_literal[0..6]);
+    let word = first_word_new(&my_string_literal[..]);
+
+    // Because string literals *are* string slices already,
+    // this works too, without the slice syntax!
+    let word = first_word_new(my_string_literal);
+}
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
+
+fn first_word_new(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
