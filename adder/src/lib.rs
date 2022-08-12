ef47d0e069d7bc291509c90ebeadef46c15ee286
Alexandre Amor <dev@ralex.fr>
2022-08-12 18:19:18 +0200

    Ending chapter 11

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -16,6 +16,11 @@ pub fn add_two(a: i32) -> i32 {
     a + 2
 }
 
+#[allow(dead_code)]
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
+}
+
 pub fn greeting(name: &str) -> String {
     format!("Hello {}!", name)
 }
@@ -66,5 +71,25 @@ mod tests {
             "Greeting did not contain name, value was `{}`",
             result
         );
+    }
+
+    #[test]
+    fn add_two_and_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn add_three_and_two() {
+        assert_eq!(5, add_two(3));
+    }
+
+    #[test]
+    fn one_hundred() {
+        assert_eq!(102, add_two(100));
+    }
+
+    #[test]
+    fn internal() {
+        assert_eq!(4, internal_adder(2, 2));
     }
 }
