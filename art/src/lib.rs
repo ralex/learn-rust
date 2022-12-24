f33b7e61e0fc1cd27cce523846066e01ed1dc265
Alexandre Amor <dev@ralex.fr>
2022-12-24 23:05:32 +0100

    Ending chapter 14

---
+++ art/src/lib.rs
@@ -0,0 +1,41 @@
+//! # Art
+//!
+//! A library for modeling artistic concepts.
+
+pub use self::kinds::PrimaryColor;
+pub use self::kinds::SecondaryColor;
+pub use self::utils::mix;
+
+pub mod kinds {
+    /// The primary colors according to the RYB color model.
+    pub enum PrimaryColor {
+        Red,
+        Yellow,
+        Blue,
+    }
+
+    /// The secondary colors according to the RYB color model.
+    #[derive(Debug)]
+    pub enum SecondaryColor {
+        Orange,
+        Green,
+        Purple,
+    }
+}
+
+pub mod utils {
+    use crate::kinds::*;
+
+    /// Combines two primary colors in equal amounts to create
+    /// a secondary color.
+    pub fn mix(c1: PrimaryColor, c2: PrimaryColor) -> SecondaryColor {
+        match (c1, c2) {
+            (PrimaryColor::Red, PrimaryColor::Yellow) => SecondaryColor::Orange,
+            (PrimaryColor::Red, PrimaryColor::Blue) => SecondaryColor::Purple,
+            (PrimaryColor::Yellow, PrimaryColor::Blue) => SecondaryColor::Green,
+            (PrimaryColor::Blue, _) => todo!(),
+            (PrimaryColor::Red, _) => todo!(),
+            (PrimaryColor::Yellow, _) => todo!(),
+        }
+    }
+}
