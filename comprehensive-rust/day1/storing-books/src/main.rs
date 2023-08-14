1a329aa5f14fab7f40f3cf1e4b4f8bfb3df964e0
Alexandre Amor <dev@ralex.fr>
2023-08-14 18:33:00 +0200

    Adding tests from solution to validate exercise

--- comprehensive-rust/day1/storing-books/src/main.rs
+++ comprehensive-rust/day1/storing-books/src/main.rs
@@ -82,3 +82,53 @@ fn main() {
     println!("The library has {} books", library.len());
     library.print_books();
 }
+
+
+#[test]
+fn test_library_len() {
+    let mut library = Library::new();
+    assert_eq!(library.len(), 0);
+    assert!(library.is_empty());
+
+    library.add_book(Book::new("Lord of the Rings", 1954));
+    library.add_book(Book::new("Alice's Adventures in Wonderland", 1865));
+    assert_eq!(library.len(), 2);
+    assert!(!library.is_empty());
+}
+
+#[test]
+fn test_library_is_empty() {
+    let mut library = Library::new();
+    assert!(library.is_empty());
+
+    library.add_book(Book::new("Lord of the Rings", 1954));
+    assert!(!library.is_empty());
+}
+
+#[test]
+fn test_library_print_books() {
+    let mut library = Library::new();
+    library.add_book(Book::new("Lord of the Rings", 1954));
+    library.add_book(Book::new("Alice's Adventures in Wonderland", 1865));
+    // We could try and capture stdout, but let us just call the
+    // method to start with.
+    library.print_books();
+}
+
+#[test]
+fn test_library_oldest_book() {
+    let mut library = Library::new();
+    assert!(library.oldest_book().is_none());
+
+    library.add_book(Book::new("Lord of the Rings", 1954));
+    assert_eq!(
+        library.oldest_book().map(|b| b.title.as_str()),
+        Some("Lord of the Rings")
+    );
+
+    library.add_book(Book::new("Alice's Adventures in Wonderland", 1865));
+    assert_eq!(
+        library.oldest_book().map(|b| b.title.as_str()),
+        Some("Alice's Adventures in Wonderland")
+    );
+}
