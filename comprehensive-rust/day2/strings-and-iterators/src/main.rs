d030b35a86fd87941c0a85da1a584c336a874cf0
Alexandre Amor <dev@ralex.fr>
2023-09-20 19:10:33 +0200

    Add Strings and Iterators exercise

---
+++ comprehensive-rust/day2/strings-and-iterators/src/main.rs
@@ -0,0 +1,51 @@
+// TODO: remove this when you're done with your implementation.
+#![allow(unused_variables, dead_code)]
+
+pub fn prefix_matches(prefix: &str, request_path: &str) -> bool {
+    let mut chars_request_path = request_path.split('/');
+
+    for split in prefix.split('/') {
+        let iter = chars_request_path.next();
+        match iter {
+            Some(x) if x != split && split != "*" => { return false },
+            Some(split) => {},
+            None => { return false }
+        };
+    }
+    true
+}
+
+#[test]
+fn test_matches_without_wildcard() {
+    assert!(prefix_matches("/v1/publishers", "/v1/publishers"));
+    assert!(prefix_matches("/v1/publishers", "/v1/publishers/abc-123"));
+    assert!(prefix_matches("/v1/publishers", "/v1/publishers/abc/books"));
+
+    assert!(!prefix_matches("/v1/publishers", "/v1"));
+    assert!(!prefix_matches("/v1/publishers", "/v1/publishersBooks"));
+    assert!(!prefix_matches("/v1/publishers", "/v1/parent/publishers"));
+}
+
+#[test]
+fn test_matches_with_wildcard() {
+    assert!(prefix_matches(
+        "/v1/publishers/*/books",
+        "/v1/publishers/foo/books"
+    ));
+    assert!(prefix_matches(
+        "/v1/publishers/*/books",
+        "/v1/publishers/bar/books"
+    ));
+    assert!(prefix_matches(
+        "/v1/publishers/*/books",
+        "/v1/publishers/foo/books/book1"
+    ));
+
+    assert!(!prefix_matches("/v1/publishers/*/books", "/v1/publishers"));
+    assert!(!prefix_matches(
+        "/v1/publishers/*/books",
+        "/v1/publishers/foo/booksByAuthor"
+    ));
+}
+
+fn main() {}
